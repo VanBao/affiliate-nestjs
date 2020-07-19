4cec63e70c0c8c448f71538e0438f8b6bb4c4b2d
tameplus <tameplus@gmail.com>
2020-07-20 06:48:28 +0700

    update

---
+++ src/database-connection.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class DatabaseConnectionService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      name: 'default',
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.Dimport { Injectable } from '@nestjs/common';
+      import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+      
+      @Injectable()
+      export class DatabaseConnectionService implements TypeOrmOptionsFactory {
+        createTypeOrmOptions(): TypeOrmModuleOptions {
+          return {
+            name: 'default',
+            type: 'mysql',
+            host: process.env.DATABASE_HOST,
+            port: Number(process.env.DATABASE_PORT),
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_DB,
+            synchronize: true,
+            dropSchema: true,
+            logging: true,
+            entities: ['dist/**/*.entity.js'],
+          };
+        }
+      }
+      ATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DB,
+      synchronize: true,
+      dropSchema: true,
+      logging: true,
+      entities: ['dist/**/*.entity.js'],
+    };
+  }
+}
